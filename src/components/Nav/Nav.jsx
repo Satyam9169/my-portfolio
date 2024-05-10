bb1918a3c160693af1879fa7fb4aa9a9f909a97c
Satyam Agrahari <satyamagrahari952@gmail.com>
2024-05-10 23:46:22 +0530

    Navigation part is done

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -6,16 +6,16 @@ const Nav = () => {
   return (
     <>
       <nav className='navcontainer'>
-        <div className='logo'><p>my Portfilio</p></div>
+        <div className='logo'><p>My Portfilio</p></div>
         <div className='nav-menu'>
           <ul className='nav-item'>
-            <li><Link>Home</Link></li>
-            <li><Link>About us</Link></li>
-            <li><Link>Skills</Link></li>
-            <li><Link>Experience</Link></li>
-            <li><Link>Qualification</Link></li>
-            <li><Link>Project</Link></li>
-            <li><Link>Contact us</Link></li>
+            <li><Link to="/" className='nav-link'>Home</Link></li>
+            <li><Link to="/" className='nav-link'>About us</Link></li>
+            <li><Link to="/" className='nav-link'>Skills</Link></li>
+            <li><Link to="/" className='nav-link'>Experience</Link></li>
+            <li><Link to="/" className='nav-link'>Qualification</Link></li>
+            <li><Link to="/" className='nav-link'>Project</Link></li>
+            <li><Link to="/" className='nav-link'>Contact us</Link></li>
           </ul>
         </div>
       </nav>
