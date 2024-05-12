65eb092b347fcd211c07705b3b844a65e3af81d5
Satyam Agrahari <satyamagrahari952@gmail.com>
2024-05-12 23:22:34 +0530

    created-responsive-design-for-navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './Nav.css'
 import { Link } from 'react-router-dom'
 
+
 const Nav = () => {
   return (
     <>
       <nav className='navcontainer'>
-        <div className='logo'><p>My Portfilio</p></div>
+        <div className='logo'><p>&lt;My Portfilio&#47;&gt;</p></div>
         <div className='nav-menu'>
           <ul className='nav-item'>
             <li><Link to="/" className='nav-link'>Home</Link></li>
@@ -16,6 +17,7 @@ const Nav = () => {
             <li><Link to="/" className='nav-link'>Qualification</Link></li>
             <li><Link to="/" className='nav-link'>Project</Link></li>
             <li><Link to="/" className='nav-link'>Contact us</Link></li>
+            <li><i className="fa-solid fa-bars" width={20} height={20}></i></li>
           </ul>
         </div>
       </nav>
